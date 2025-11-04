899d21aa3384e640f3c04f866f84c2b0790cfaee
ItsGetWreckt <samanyu.rayala@gmail.com>
2025-11-03 22:26:55 -0800

    Added netherite carrot and enchanted golden apple on a stick

--- src/main/java/com/therealsam/upgradedpigscarrots/SamsUpgradedPigsCarrots.java
+++ src/main/java/com/therealsam/upgradedpigscarrots/SamsUpgradedPigsCarrots.java
@@ -1,5 +1,6 @@
 package com.therealsam.upgradedpigscarrots;
 
+import com.therealsam.upgradedpigscarrots.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,6 +12,6 @@ public class SamsUpgradedPigsCarrots implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
