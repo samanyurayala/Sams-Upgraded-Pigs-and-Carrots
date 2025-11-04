ea8bd2e9fdb28ca6163e00daa5d6668dee8e6745
ItsGetWreckt <samanyu.rayala@gmail.com>
2025-11-03 19:31:43 -0800

    Initial commit

---
+++ src/main/java/com/therealsam/upgradedpigscarrots/SamsUpgradedPigsCarrots.java
@@ -0,0 +1,16 @@
+package com.therealsam.upgradedpigscarrots;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SamsUpgradedPigsCarrots implements ModInitializer {
+	public static final String MOD_ID = "upgradedpigscarrots";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/therealsam/upgradedpigscarrots/SamsUpgradedPigsCarrotsClient.java
@@ -0,0 +1,10 @@
+package com.therealsam.upgradedpigscarrots;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SamsUpgradedPigsCarrotsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
